e5593613bb59cdfc318aa1a05e4142342345f9a7
Manuel <mgnr7@gmail.com>
2019-09-10 09:39:51 -0600

    Actividad 2

--- ejemplos/pthreads/hello_i_of_w/hello_i_of_w.c
+++ ejemplos/pthreads/hello_i_of_w/hello_i_of_w.c
@@ -14,6 +14,69 @@ typedef struct
 	shared_data_t* shared_data;
 } private_data_t;
 
+int create_threads(shared_data_t* shared_data);
+void* run(void* data);
+
+int main (int argc, char*argv[])
+{
+	shared_data_t* shared_data = (shared_data_t*) calloc(1, sizeof(shared_data_t));
+	if(shared_data == NULL)
+		return (void)fprintf(stderr, "error: could not allocate memory\n"), 2;
+	
+	shared_data->thread_count = sysconf(_SC_NPROCESSORS_ONLN);
+	if(argc >=2)
+		shared_data->thread_count = strtoull(argv[1], NULL, 10);
+	
+	struct timespec start_time;	
+	clock_gettime(CLOCK_MONOTONIC, &start_time);						/*Mide el tiempo y lo guarda en el registro*/	
+	
+	int error = create_threads(shared_data);
+	if(error)
+		return error;
+		
+	struct timespec end_time;
+	clock_gettime(CLOCK_MONOTONIC, &end_time);
+	
+	double elapsed_seconds = end_time.tv_sec - start_time.tv_sec + 1e-9 *(end_time.tv_nsec - start_time.tv_nsec);
+	printf("Hellow execution time %.9lfs\n", elapsed_seconds);
+	
+	return 0;
+}
+
+/*Si el thread principal se acaba, se acaba el programa*/
+
+int create_threads(shared_data_t* shared_data)
+{
+    pthread_t* threads = (pthread_t*) malloc(shared_data->thread_count * sizeof(pthread_t)); 	/*malloc retorna un puntero void y reserva (thread_count * sizeof(pthread_t)) espacio en memoria*/
+	
+	if(threads == NULL)
+		return (void)fprintf(stderr, "error: could not allocate memory for %zu threads \n", shared_data->thread_count), 1;
+	
+	private_data_t* private_data = (private_data_t*) calloc(shared_data->thread_count, sizeof(private_data_t));	/*Calloc reserva memoria y rellena con ceros*/
+	
+	if(private_data == NULL)
+		return (void)fprintf(stderr, "error: could not allocate memory for %zu threads \n", shared_data->thread_count), 3;
+		
+	
+	for(size_t index = 0; index < shared_data->thread_count ; ++index)							/*Primera fase: Creacion de threads*/
+	{
+		private_data[index].thread_num = index;
+		private_data[index].shared_data = shared_data;
+		pthread_create(&threads[index], NULL, run, &private_data[index]);
+	}
+	
+	printf("Hellow World from main thread\n");
+	
+	for(size_t index = 0; index < shared_data->thread_count ; ++index)							/*Segunda fase: Espera por cada uno de los threads*/	
+		pthread_join(threads[index], NULL); 														
+	
+	
+	free(private_data);				/*Se elimina en el orden inverso en el que se crea la memoria dinamica*/
+	free(shared_data);
+	free(threads);	
+	return 0;
+}
+
 void* run(void* data)
 {
 	private_data_t* private_data = (private_data_t*)data;
@@ -24,58 +87,3 @@ void* run(void* data)
 	printf("Hellow World from secondary thread %zu of %zu\n", thread_num, thread_count); 
 	return NULL;
 }
-
-int main (int argc, char*argv[])
-{
-	size_t thread_count = sysconf(_SC_NPROCESSORS_ONLN);
-	
-	if(argc >=2)
-		thread_count = strtoull(argv[1], NULL, 10);
-	
-	pthread_t* threads = (pthread_t*) malloc(thread_count * sizeof(pthread_t)); 	/*malloc retorna un puntero void y reserva (thread_count * sizeof(pthread_t)) espacio en memoria*/
-	
-	if(threads == NULL)
-		return (void)fprintf(stderr, "error: could not allocate memory for %zu threads \n", thread_count), 1;
-	
-	shared_data_t* shared_data = (shared_data_t*) calloc(1, sizeof(shared_data_t));
-	
-	if(shared_data == NULL)
-		return (void)fprintf(stderr, "error: could not allocate memory for %zu threads \n", thread_count), 2;
-
-	shared_data->thread_count = thread_count;
-	
-	private_data_t* private_data = (private_data_t*) calloc(thread_count, sizeof(private_data_t));	/*Calloc reserva memoria y rellena con ceros*/
-	
-	if(private_data == NULL)
-		return (void)fprintf(stderr, "error: could not allocate memory for %zu threads \n", thread_count), 3;
-		
-	struct timespec start_time;	
-	clock_gettime(CLOCK_MONOTONIC, &start_time);						/*Mide el tiempo y lo guarda en el registro*/	
-	
-	for(size_t index = 0; index < thread_count ; ++index)							/*Primera fase: Creacion de threads*/
-	{
-		private_data[index].thread_num = index;
-		private_data[index].shared_data = shared_data;
-		pthread_create(&threads[index], NULL, run, &private_data[index]);
-	}
-	
-	printf("Hellow World from main thread\n");
-	
-	for(size_t index = 0; index < thread_count ; ++index)							/*Segunda fase: Espera por cada uno de los threads*/	
-		pthread_join(threads[index], NULL); 														
-	
-	struct timespec end_time;
-	clock_gettime(CLOCK_MONOTONIC, &end_time);
-	
-	double elapsed_seconds = end_time.tv_sec - start_time.tv_sec + 1e-9 *(end_time.tv_nsec - start_time.tv_nsec);
-	
-	printf("Hellow execution time %.9lfs\n", elapsed_seconds);
-	
-	
-	free(private_data);				/*Se elimina en el orden inverso en el que se crea la memoria dinamica*/
-	free(shared_data);
-	free(threads);	
-	return 0;
-}
-
-/*Si el thread principal se acaba, se acaba el programa*/
