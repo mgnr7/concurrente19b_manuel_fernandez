2fa5e266dfe037c9475d6d1edfb1ef4eb10e9349
Manuel <malgnr7@gmail.com>
2019-11-06 00:13:19 -0600

    ejercicios resueltos

---
+++ ejercicios/pthreads/trapezoidal_pthreads/trapezoidal_pthreads.c
@@ -0,0 +1,165 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+#include <math.h>
+#include <pthread.h>
+
+typedef struct
+{
+	double a;		/*punto inferior*/
+	double b;		/*punto superior*/
+	int n;			/*cantidad de trapecios*/
+	double result;	/*resultado*/
+	int thread_count;
+	pthread_mutex_t mutex;
+}shared_data_t;
+
+
+typedef struct
+{
+	double bottom;			/*punto inferior del intervalo en el que va a calcular el thread*/
+	double top;				/*punto superior del intervalo en el que va a calcular el thread*/
+	int cant_trapecios;		/*cantidad de trapecios para el thread*/
+	double thread_result;	/*resultado del thread*/
+	shared_data_t* shared_data;
+}private_data_t;
+
+
+int analyze_arguments(int argc, char*argv[], shared_data_t* shared_data);
+int create_threads(shared_data_t* shared_data);
+void* trapezoidal_area(void* data);	/*Calcula la integral de la funcion "f" en el intervalo [a, b] con "n" trapecios*/
+double imagen(double x);							/*Devuelve la imagen f(x)*/
+
+
+int main (int argc, char*argv[])
+{
+	shared_data_t* shared_data = (shared_data_t*) calloc(1, sizeof(shared_data_t));
+	if(shared_data == NULL)
+		return (void)fprintf(stderr, "error: could not allocate memory\n"), 2;
+
+	int error = analyze_arguments(argc, argv, shared_data);
+	if(error == 0)
+	{
+		
+		pthread_mutex_init(&shared_data->mutex,NULL);
+		struct timespec start_time;	
+		clock_gettime(CLOCK_MONOTONIC, &start_time);	
+							
+		int error = create_threads(shared_data);
+		if(error == 0)
+		{
+			printf("Total Area: %lf \n", shared_data->result);
+			struct timespec end_time;
+			clock_gettime(CLOCK_MONOTONIC, &end_time);	
+			double elapsed_seconds = end_time.tv_sec - start_time.tv_sec + 1e-9 *(end_time.tv_nsec - start_time.tv_nsec);
+			printf("Calculations time %.9lfs\n", elapsed_seconds);	
+		}	
+		
+	}
+	pthread_mutex_destroy(&shared_data->mutex);
+	free(shared_data);
+	return error;
+}
+
+int analyze_arguments(int argc, char*argv[], shared_data_t* shared_data)
+{
+	if(argc != 5)
+	{
+		fprintf(stderr, "\n	usage: a, b, n, thread_count \n");	
+		return 1;
+	}
+	if(sscanf(argv[1],"%lf", &shared_data->a) != 1)
+	{
+		fprintf(stderr, "\n	invalid a value: %s\n", argv[1]);	
+		return 2;
+	}	
+	if(sscanf(argv[2],"%lf", &shared_data->b) != 1 || shared_data->a > shared_data->b)
+	{	
+		fprintf(stderr, "invalid b value: %s\n", argv[2]);
+		return 2;
+	}
+	if(sscanf(argv[3],"%d", &shared_data->n) != 1 || shared_data->n == 0)
+	{
+		fprintf(stderr, "invalid n value: %s\n", argv[3]);
+		return 3;
+	}
+	if(sscanf(argv[4],"%d", &shared_data->thread_count) != 1)
+	{
+		fprintf(stderr, "invalid n value: %s\n", argv[4]);
+		return 4;
+	}
+	if(shared_data->thread_count == 0)
+	{
+		shared_data->thread_count = sysconf(_SC_NPROCESSORS_ONLN);
+	}
+	
+	return EXIT_SUCCESS;
+}
+
+int create_threads(shared_data_t* shared_data)
+{
+	int number_of_threads = shared_data->thread_count;
+	pthread_t* threads = (pthread_t*) malloc(number_of_threads * sizeof(pthread_t)); 
+	
+	if(threads == NULL)
+		return (void)fprintf(stderr, "error: could not allocate memory for %d threads \n", number_of_threads), 1;
+	
+	private_data_t* private_data = (private_data_t*) calloc(number_of_threads, sizeof(private_data_t));	
+	
+	if(private_data == NULL)
+		return (void)fprintf(stderr, "error: could not allocate memory for %d threads \n", number_of_threads), 3;
+		
+	/*hay que dividir el intervalo entre los threads*/
+	
+	for(int index = 0; index < number_of_threads ; ++index)							
+	{
+		private_data[index].bottom = shared_data->a + (index * ((shared_data->b - shared_data->a) / number_of_threads));
+		private_data[index].top = shared_data->a + ((index + 1) * ((shared_data->b - shared_data->a)/ number_of_threads));
+		private_data[index].cant_trapecios = (shared_data->n / shared_data->thread_count);
+		private_data[index].shared_data = shared_data;
+		pthread_create(&threads[index], NULL, trapezoidal_area, &private_data[index]);
+	}
+
+	for(int index = 0; index < number_of_threads ; ++index)								
+		pthread_join(threads[index], NULL); 														
+	
+	
+	free(private_data);				
+	free(threads);	
+	return 0;
+}
+
+void* trapezoidal_area(void* data)
+{
+	 private_data_t* private_data = (private_data_t*)data;
+	 shared_data_t* shared_data = private_data->shared_data;
+	 double a = private_data->bottom;
+	 double b = private_data->top;
+	 int n = private_data->cant_trapecios;
+	 double trapezoid_width;
+	 trapezoid_width = (b - a) / n;
+	 
+	 double thr_result;
+	 thr_result = imagen(a);
+	 thr_result += imagen(b);
+	 
+	 for(double index = 1; index < n; index++)
+	 {
+		 thr_result = thr_result + imagen(a + index * trapezoid_width);
+	 } 
+	 
+	 pthread_mutex_lock(&shared_data->mutex);
+	 private_data->shared_data->result += (trapezoid_width / 2) * 2*thr_result;
+	 pthread_mutex_unlock(&shared_data->mutex);
+	 
+	 return NULL;
+}
+
+double imagen(double x)
+{
+	/*Evaluar x en la parabola y retornar el valor obtenido*/
+	 double y;
+	 y = (x*x);
+	 return y;
+}
---
+++ ejercicios/pthreads/trapezoidal_serial/trapezoidal_serial.c
@@ -0,0 +1,90 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+#include <math.h>
+
+double trapezoidal_area(double a, double b, int n);	/*Calcula la integral de la funcion "f" en el intervalo [a, b] con "n" trapecios*/
+double imagen(double x);							/*Devuelve la imagen f(x)*/
+
+int main (int argc, char*argv[])
+{
+
+	double a;		/*punto inferior*/
+	double b;		/*punto superior*/
+	int n;			/*cantidad de trapecios*/
+	double result;	/*resultado*/
+	
+	if(argc != 4)
+	{
+		fprintf(stderr, "\n	usage: a, b, n \n");	
+		return 1;
+	}
+
+	if(sscanf(argv[1],"%lf", &a) != 1)
+	{
+		fprintf(stderr, "\n	invalid a value: %s\n", argv[1]);	
+		return 2;
+	}
+		
+	if(sscanf(argv[2],"%lf", &b) != 1 || a > b)
+	{	
+		fprintf(stderr, "invalid b value: %s\n", argv[2]);
+		return 2;
+	}
+	if(sscanf(argv[3],"%d", &n) != 1 || n == 0)
+	{
+		fprintf(stderr, "invalid n value: %s\n", argv[3]);
+		return 3;
+	}
+
+		struct timespec start_time;	
+		clock_gettime(CLOCK_MONOTONIC, &start_time);						
+		
+		result = trapezoidal_area(a, b, n);		/*Invocacion a trapezoidal_Area*/
+		printf("Total Area: %lf \n", result);
+		struct timespec end_time;
+		clock_gettime(CLOCK_MONOTONIC, &end_time);	
+		double elapsed_seconds = end_time.tv_sec - start_time.tv_sec + 1e-9 *(end_time.tv_nsec - start_time.tv_nsec);
+		printf("Calculations time %.9lfs\n", elapsed_seconds);
+	
+	return 0;
+}
+
+double trapezoidal_area(double a, double b, int n)
+{
+	/*Determinar el ancho de cada trapecio:
+	 * (b - a) / n
+	 * 
+	 *Hay que ejecutar n sumas:
+	 * -Primero calcular ancho/2
+	 * -Despues calcular f(a) y f(b):
+	 * 	result = f(a) + f(b)
+	 * -Despues ejecutar las sumas 2*f(a + ancho_trapecio) + ... + 2*f(b - ancho_trapecio)
+	 * Estas sumas se hacen dentro de un ciclo for que se incrementa cada ancho_trapecio, mientras sea menor a b inciando en a+ancho_trapecio
+	 * for(index = ancho_trapecio; index < b; index + ancho_trapecio)
+	 * -Finalemente al resultado de las sumas hay que multiplicarlo por (ancho_trapecio / 2)
+	 * */	 
+	 double trapezoid_width;
+	 trapezoid_width = (b - a) / n;
+	 
+	 double result;
+	 result = imagen(a);
+	 result += imagen(b);
+	 
+	 for(double index = 1; index < n; index++)
+	 {
+		 result = result +  imagen(a+index * trapezoid_width);
+	 } 
+	 
+	 
+	 return (trapezoid_width / 2) * 2*result;
+}
+
+double imagen(double x)
+{
+	/*Evaluar x en la parabola y retornar el valor obtenido*/
+	 double y;
+	 y = ((x*x));
+	 return y;
+}
