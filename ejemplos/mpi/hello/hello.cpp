e57197b4237fdd3576f008a5d03d4732ef23bc9a
Manuel Fernandez Carrillo --CI-0117-02 <B72892@arenal.ecci.ucr.ac.cr>
2019-11-12 21:50:42 -0600

    archivos mpi

---
+++ ejemplos/mpi/hello/hello.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <mpi.h>
+
+int main(int argc, char* argv[])
+{
+	MPI_Init(&argc, &argv);
+	int my_rank = -1;
+	int process_count = -1;
+	
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);	
+	MPI_Comm_size(MPI_COMM_WORLD, &process_count);
+	
+	char hostname[MPI_MAX_PROCESSORS_NAME];
+	int hostname_length = -1;
+	MPI_Get_processor_name(hostname, hostname_length);
+	
+	
+	
+	std::cout << "hello from main thread "<< my_rank << " of "<< process_count << " on " << hostname << std::endl;
+	
+	MPI_Finalize();
+}
